f751810d26b185e3bba953722accf0ae8233be98
ValSGav <valentin-s-gavrilov@yandex.ru>
2023-07-04 17:23:38 +0300

    2023_07_04

---
+++ Lesson3/Worker.java
@@ -0,0 +1,21 @@
+package Lesson3;
+
+import java.util.Iterator;
+
+public class Worker implements Iterator<Worker> {
+
+    
+
+    @Override
+    public boolean hasNext() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'hasNext'");
+    }
+
+    @Override
+    public Worker next() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'next'");
+    }
+        
+}
